2b268e6fa13e7e7ac11c26f44026aba9c7c886f5
DT2004 <44294436+DT2004@users.noreply.github.com>
2018-10-20 12:18:28 +0300

    Update simple_arithmetic.py

--- simple_arithmetic.py
+++ simple_arithmetic.py
@@ -1,12 +1,11 @@
-print(1+2)
+print(1+2) # this function is for addition
 
-print("1+2")
-
-print(30%2)
+print(30%2)# this is for finding out the remainder
 
 print(1984928347/10823)
-
+#using vaariables
 a = 10
 b = 2
 print(a/b)
 #prints out to 5.0 not 5
+print(a/b)
